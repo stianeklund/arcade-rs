f73c4e21508c4a42aab67d4641958a55708ed0d7
Stian Eklund <stianeklund@users.noreply.github.com>
2016-10-24 22:56:23 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate sdl2_image;
 #[macro_use]
 mod phi;
 mod views;
-// http://jadpole.github.io/arcaders/arcaders-1-6
+
 fn main() {
     ::phi::spawn("ArcadeRS Shooter", |phi| {
         Box::new(::views::ShipView::new(phi))
